20e9edd3f0ad1dd22b82e353eefe922ccd22feff
Ksu <ksenya-7@yandex.ru>
2020-11-16 00:54:35 +0300

    cleaning of file'filter'

--- js/filter.js
+++ js/filter.js
@@ -35,18 +35,8 @@ const filterByFeatures = (element) => {
   const features = element.offer.features;
   const checkedInputs = housingFeatures.querySelectorAll(`input[type=checkbox]:checked`);
   const checkedFeatures = [].map.call(checkedInputs, (input) => input.value);
-  // let result = true;
+
   return checkedFeatures.every((el) => features.includes(el));
-  // console.log(checkedFeatures);
-  // for (let i = 0; i < checkedInputs.length; i++) {
-  //   console.log(checkedInputs[i].value);
-  //   for (let j = 0; j < checkedFeatures.length; j++) {
-  //     result = checkedFeatures.some(checkedInputs[i].value === element.offer.features[j]);
-  //   }
-  // }
-  // console.log(result);
-  // return checkedInputs[i].value;
-  // return result;
 };
 
 const debouncedRenderSetOfPins = window.debounce(window.renderPins);
@@ -54,17 +44,8 @@ const debouncedRenderSetOfPins = window.debounce(window.renderPins);
 const filtersHandler = (elements) => {
   mapFilters.addEventListener(`change`, () => {
     const filteredPins = elements.filter(filterByType).filter(filterByPrice).filter(filterByRooms).filter(filterByGuests).filter(filterByFeatures);
-    // console.log(filteredPins);
     debouncedRenderSetOfPins(filteredPins);
   });
 };
 
 window.filtersHandler = filtersHandler;
-
-// Доработайте модуль, отрисовывающий метки, таким образом, чтобы отрисованные на карте
-// метки можно было фильтровать с помощью фильтров, расположенных в блоке .map__filters.
-// После фильтрации должны показываться те метки из набора данных, которые подходят под
-// выбранные фильтры. Метки, отрисованные до этого нужно убрать. Все выбранные фильтры
-// применяются вместе: один фильтр не отменяет другие, выбранные до него. Например, после
-// выбора типа жилья можно указать диапазон стоимости и дополнения и в этом случае,
-// на карте должны показываться только те метки, которые подходят под все условия.
